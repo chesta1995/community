0361704ec52d78b4d3f83bf45fb78aaa5d149995
chesta1995 <47781481+chesta1995@users.noreply.github.com>
2019-12-30 15:32:10 +0900

    first commit

---
+++ src/main/java/com/ch/community/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.ch.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @ClassName Hello
+ * @Description TODO
+ * @Date 2019/12/30 0030 14:56
+ * @Created by Administrator
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello (@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name",name + "2");
+        return "hello";
+    }
+}
